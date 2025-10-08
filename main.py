d36e05fa7f129ce8f573957c31251ff336b6b2f5
walberluis <wlsp@ic.ufal.br>
2025-10-08 17:43:47 -0300

    commit inicial

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ def main():
         print("❌ Erro: Arquivo 'entrada.txt' não encontrado.")
     except Exception as e:
         print(f"❌ Ocorreu um erro: {e}")
+        
 
 
 if __name__ == "__main__":
