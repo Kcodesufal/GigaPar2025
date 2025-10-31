e64133f636505b82329caf5b459a4432fdd310e2
walberluis <wlsp@ic.ufal.br>
2025-10-31 18:31:54 -0300

    adicionando t4 e t8 na main

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def write_assembly_to_file(asm_code, filename="output.s"):
 
 
 def main():
-    testes = ["entrada.txt", "t1.par", "t2.par","t3.par","t5.par","t6.par","t7.par","t8.par"]
+    testes = ["entrada.txt", "t1.par", "t2.par","t3.par", "t4.par", "t5.par","t6.par","t7.par", "t8.par"]
     for test in testes:
         try:
             with open("tests/" + test, "r", encoding="utf-8") as f:
